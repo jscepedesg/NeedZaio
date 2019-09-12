bef3d36a73020e6559ab5f1e8f554426b80a745a
Sebastian Cespedes <jahirsebastian@hotmail.com>
2019-09-12 16:39:47 -0500

    Creacion de endpoint

--- common/models/app-user-tb.js
+++ common/models/app-user-tb.js
@@ -2,4 +2,16 @@
 
 module.exports = function(Appusertb) {
 
+  /**
+ * Permitira registrar los datos basicos del usuario, una identificacion y informacion de contacto.
+ * @param {object} data datos del registro
+ * @param {Function(Error, object)} callback
+ */
+
+  Appusertb.prototype.registrar_usuario = function(data, callback) {
+    var result;
+    // TODO
+    callback(null, result);
+  };
+
 };
